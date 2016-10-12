ba228b6c7ad776b238a058774dbf8012b30ca975
Robert Kirsz <robert.kirsz@gmail.com>
2016-10-12 19:48:21 +0200

    Usunięcie PHP i zastąpienie go JS-em

--- js/main.js
+++ js/main.js
@@ -165,6 +165,77 @@ $(function() {
 		}
 	});
 
+	(function() {
+		$.getJSON('../xml/portfolio.json', function(data) {
+			$.each(data, function(key, value) {
+				var nazwa = value.nazwa;
+				var adres = value.adres;
+				var opis = value.opis;
+				var desktop = value.screenshot.desktop;
+				var mobile = value.screenshot.mobile;
+				var responsywnosc = value.responsywnosc;
+				var uklad = null
+				var bootstrap1 = null
+				var bootstrap2 = null
+
+				if (key % 2 === 0) {
+						uklad = desktop === '' ? 'mobile_only left' : 'responsive left';
+						bootstrap1 = 'col-md-push-8';
+						bootstrap2 = 'col-md-pull-4';
+				} else {
+						uklad = desktop === '' ? 'mobile_only right' : 'responsive right';
+						bootstrap1 = '';
+						bootstrap2 = '';
+				}
+
+				function generateResponsivenessIcons (desktop, tablet, mobile) {
+					var $responsiveIcons = $('<h3 />', { class: 'responsive-icons' });
+					$responsiveIcons.append($('<i />', { class: 'fa fa-desktop ' + responsywnosc.desktop }));
+					$responsiveIcons.append($('<i />', { class: 'fa fa-tablet ' + responsywnosc.tablet }));
+					$responsiveIcons.append($('<i />', { class: 'fa fa-mobile ' + responsywnosc.desktop }));
+					return $responsiveIcons;
+				}
+
+				var $portfolioItem = $('<div />', { class: 'row portfolio-item ' + uklad });
+				var $description = $('<div />', { class: 'description col-md-4 ' + bootstrap1 });
+				var $browsers = $('<div />', { class: 'browsers col-md-8 ' + bootstrap2 });
+
+				$description.append($('<h1 />', { text: nazwa }));
+				$description.append(
+					generateResponsivenessIcons(responsywnosc.desktop, responsywnosc.tablet, responsywnosc.mobile)
+				);
+				$description.append($('<p />', { text: opis }));
+				$description.append(
+					$('<a />', {
+						href: 'http://' + adres,
+						type: 'button',
+						class: 'btn btn-info',
+						text: 'Zobacz projekt na żywo'
+					})
+				);
+
+				$phone = $('<div />', { class: 'phone' });
+				$top = $('<div class="top"></div>');
+				$screen = $('<div class="screen"><img src="img/portfolio/' + mobile + '" alt="' + nazwa + ' Mobile Version"></div>');
+				$bottom = $('<div class="bottom"></div>');
+				$phone.append($top);
+				$phone.append($screen);
+				$phone.append($bottom);
+
+				$desktop = $('<div class="desktop"><div class="top"><i class="fa fa-arrow-left"></i><i class="fa fa-arrow-right"></i><i class="fa fa-refresh"></i><i class="fa fa-home"></i><div class="address"><a href="http://' + adres + '">' + adres + '</a></div></div><div class="screen"><img src="img/portfolio/' + desktop + '" alt="' + nazwa + ' Desktop Version"></div></div>');
+
+				if (desktop !== '') $browsers.append($desktop);
+				if (mobile !== '') $browsers.append($phone);
+
+				$portfolioItem
+					.append($description)
+					.append($browsers);
+
+				$('#main_portfolio .container').append($portfolioItem);
+			});
+		});
+	})();
+
 });
 
 $(window).load(function() {
